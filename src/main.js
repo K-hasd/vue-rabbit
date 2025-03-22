92b73f7400a4db5799cdeca3273032efa7206f38
K-hasd <byt925429908@163.com>
2025-03-22 20:01:34 +0800

    axios基础封装

--- src/main.js
+++ src/main.js
@@ -8,6 +8,12 @@ import router from './router'
 
 const app = createApp(App)
 
+// 测试接口函数
+import { getCategory } from '@/apis/textAPI'
+getCategory().then(res => {
+    console.log(res);
+})
+
 app.use(createPinia())
 app.use(router)
 
