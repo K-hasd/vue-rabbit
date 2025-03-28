41527f197cb34ac9a724483cb1aa554889a318a5
K-hasd <byt925429908@163.com>
2025-03-28 19:57:45 +0800

    layout-吸顶导航交互实现

--- src/main.js
+++ src/main.js
@@ -13,12 +13,6 @@ import '@/styles/common.scss'
 
 const app = createApp(App)
 
-// 测试接口函数
-import { getCategory } from '@/apis/textAPI'
-getCategory().then(res => {
-    console.log(res);
-})
-
 app.use(createPinia())
 app.use(router)
 
