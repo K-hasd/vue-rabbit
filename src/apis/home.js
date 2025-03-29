18d7c822a979834bc68aeebb8fb9fe69dce56b9a
K-hasd <byt925429908@163.com>
2025-03-29 17:34:01 +0800

    home-人气推荐业务实现

--- src/apis/home.js
+++ src/apis/home.js
@@ -14,3 +14,10 @@ export const findNewAPI = () => {
         url: '/home/new'
     })
 }
+
+// 获取人气推荐
+export const findHotAPI = () => {
+    return httpInstance({
+        url: '/home/hot'
+    })
+}
