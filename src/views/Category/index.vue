5e99650253624ce04156962583234937ff1e73e5
K-hasd <byt925429908@163.com>
2025-03-31 15:55:25 +0800

    一级分类-激活状态显示以及分类列表渲染

--- src/views/Category/index.vue
+++ src/views/Category/index.vue
@@ -3,7 +3,7 @@ import { getCategoryAPI } from '@/apis/category'
 import { ref, onMounted ,watch} from 'vue'
 import { useRoute } from 'vue-router';
 import { getBannerAPI }  from '@/apis/home'
-
+import GoodsItem from '../Home/components/GoodsItem.vue';
 
 
 //获取数据
@@ -64,7 +64,26 @@ onMounted(()=>{
             <img :src="item.imgUrl" alt="">
         </el-carousel-item>
         </el-carousel>
-  </div>
+      </div>
+      <div class="sub-list">
+  <h3>全部分类</h3>
+  <ul>
+    <li v-for="i in categoryData.children" :key="i.id">
+      <RouterLink to="/">
+        <img :src="i.picture" />
+        <p>{{ i.name }}</p>
+      </RouterLink>
+    </li>
+  </ul>
+</div>
+<div class="ref-goods" v-for="item in categoryData.children" :key="item.id">
+  <div class="head">
+    <h3>- {{ item.name }}-</h3>
+    </div>
+        <div class="body">
+            <GoodsItem v-for="good in item.goods" :goods="good" :key="good.id" />
+        </div>
+      </div>
     </div>
   </div>
 </template>
