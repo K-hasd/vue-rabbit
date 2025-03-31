590c49e7bdc056bac45ba1f92fc6730e33e64e88
K-hasd <byt925429908@163.com>
2025-03-31 15:29:28 +0800

    一级分类-轮播图实现

--- src/apis/home.js
+++ src/apis/home.js
@@ -2,16 +2,21 @@ import  httpInstance  from '@/utils/http'
 
 // 获取banner
 
-export function getBannerAPI() {
+export function getBannerAPI(params = {}) {
+    // 默认为1 商品为2
+    const { distributionSite = '1' } = params
     return httpInstance({
-        url: '/home/banner'
+        url: '/home/banner',
+        params: {
+            distributionSite
+        }
     })
 }
 
 // 获取新鲜好物
 export const findNewAPI = () => {
     return httpInstance({
-        url: '/home/new'
+        url: '/home/new',
     })
 }
 
--- src/views/Category/index.vue
+++ src/views/Category/index.vue
@@ -2,11 +2,13 @@
 import { getCategoryAPI } from '@/apis/category'
 import { ref, onMounted ,watch} from 'vue'
 import { useRoute } from 'vue-router';
+import { getBannerAPI }  from '@/apis/home'
+
+
 
 //获取数据
 const categoryData = ref({})
 const route = useRoute()
-
 const getCategory = async () => {
     const res = await getCategoryAPI(route.params.id)
     categoryData.value = res.result
@@ -24,6 +26,25 @@ onMounted(() => {
     getCategory()
 })
 
+
+// 获取banner
+
+const bannerList = ref([])
+
+const getBanner = async ()=>{
+  const res = await getBannerAPI({
+    distributionSite: '2'
+  })
+  console.log(res);
+  bannerList.value = res.result
+}
+
+onMounted(()=>{
+  getBanner()
+})
+
+
+
 </script>
 
 <template>
@@ -36,6 +57,14 @@ onMounted(() => {
           <el-breadcrumb-item>{{ categoryData.name }}</el-breadcrumb-item>
         </el-breadcrumb>
       </div>
+      <!-- 轮播图 -->
+      <div class="home-banner">
+        <el-carousel height="500px">
+        <el-carousel-item v-for="item in bannerList" :key="item.id">
+            <img :src="item.imgUrl" alt="">
+        </el-carousel-item>
+        </el-carousel>
+  </div>
     </div>
   </div>
 </template>
@@ -118,5 +147,18 @@ onMounted(() => {
   .bread-container {
     padding: 25px 0;
   }
+
+
+  .home-banner {
+  width: 1240px;
+  height: 500px;
+  margin: 0 auto;
+
+  img {
+    width: 100%;
+    height: 500px;
+  }
+}
+
 }
 </style>
