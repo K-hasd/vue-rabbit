11dec27d0aecbafab24fced32f825d23a7f8839e
K-hasd <byt925429908@163.com>
2025-04-01 17:18:35 +0800

    二级分类-基础列表渲染

--- src/views/subCategory/index.vue
+++ src/views/subCategory/index.vue
@@ -1,8 +1,8 @@
 <script setup>
-import { getCategoryFilterAPI } from '@/apis/category';
+import { getCategoryFilterAPI,getSubCategoryAPI } from '@/apis/category';
 import { useRoute } from 'vue-router';
 import { ref, onMounted} from 'vue';
-
+import  GoodsItem from '@/views/Home/components/GoodsItem.vue';
 // 获取面包屑导航数据
 const categoryData = ref({});
 const route = useRoute();
@@ -13,6 +13,23 @@ const getCategoryData = async () => {
 
 onMounted(() => {
     getCategoryData()
+})
+
+// 获取基础列表数据渲染
+const goodList = ref([]);
+const reqData = ref({
+    categoryId: route.params.id,
+    page:1,
+    pageSize:20,
+    sortFieid:'publishTime'
+})
+
+const getGoodList = async () => {
+   const res = await getSubCategoryAPI(reqData.value)
+   goodList.value = res.result.items
+}
+onMounted(() => {
+    getGoodList()
 })
 
 </script>
@@ -35,6 +52,7 @@ onMounted(() => {
       </el-tabs>
       <div class="body">
          <!-- 商品列表-->
+          <GoodsItem v-for="goods in goodList" :goods="goods" :key="goods.id"/>
       </div>
     </div>
   </div>
